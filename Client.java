81754ed7fc194660b1896983bef7b77855e5f65d
Mogetti Matteo <mmatteo0105@gmail.com>
2023-02-27 13:22:00 +0100

    client funz

--- Client.java
+++ Client.java
@@ -1,5 +1,42 @@
-public class Client{
-    public static void main(String[] args){
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.net.InetAddress;
+import java.net.SocketException;
+
+public class Client {
+    public static void main(String[] args){
+        try{
+        BufferedReader inFromUser = new BufferedReader(new InputStreamReader(System.in));
+        DatagramSocket clientSocket = new DatagramSocket();
+
+        InetAddress IPAddress = InetAddress.getByName("localhost");
+
+        byte[] sendData = new byte[1024];
+        byte[] receiveData = new byte[1024];
+
+        String sentence = inFromUser.readLine();
+        sendData = sentence.getBytes();
+
+        DatagramPacket sendPacket = new DatagramPacket(sendData, sendData.length, IPAddress, 9876);
+        clientSocket.send(sendPacket);
+
+        DatagramPacket receivePacket = new DatagramPacket(receiveData, receiveData.length);
+        clientSocket.receive(receivePacket);
+
+        String modifiedSentence = new String(receivePacket.getData());
+        System.out.println("FROM SERVER:" + modifiedSentence);
+
+        clientSocket.close();
+        }
+         catch(SocketException e){
+            System.err.println(e);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
+
